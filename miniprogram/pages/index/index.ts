3edc02de3df64ec2839b616372b510c6bb7bd5dc
zhi986338250 <986338250@qq.com>
2025-09-30 15:00:21 +0800

    图片功能从oss获取，基本界面完成

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -7,18 +7,18 @@ Page({
   onLoad() {
     // 模拟地图数据（12张地图）
     const mockMaps = [
-      { id: 'map1', name: '霓虹町', image: '/images/maps/nihong.jpg' },
-      { id: 'map2', name: '裂变峡谷', image: '/images/maps/liebian.jpg' },
-      { id: 'map3', name: '隐世修所', image: '/images/maps/yinshi.jpg' },
-      { id: 'map4', name: '亚海悬城', image: '/images/maps/yahai.jpg' },
-      { id: 'map5', name: '莲华古城', image: '/images/maps/lianhua.jpg' },
-      { id: 'map6', name: '微风岛屿', image: '/images/maps/weifeng.jpg' },
-      { id: 'map7', name: '深海明珠', image: '/images/maps/shenhai.jpg' },
-      { id: 'map8', name: '森寒冬港', image: '/images/maps/senhan.jpg' },
-      { id: 'map9', name: '盐海矿镇', image: '/images/maps/yanhai.png' },
-      { id: 'map10', name: '日落之城', image: '/images/maps/riluo.png' },
-      { id: 'map11', name: '源工重镇', image: '/images/maps/yuangong.jpg' },
-      { id: 'map12', name: '幽邃地窟', image: '/images/maps/yousui.png' }
+      { id: 'map1', name: '霓虹町', image: 'https://valorant-strategy.oss-cn-heyuan.aliyuncs.com/images/maps/nihong.jpg' },
+      { id: 'map2', name: '裂变峡谷', image: 'https://valorant-strategy.oss-cn-heyuan.aliyuncs.com/images/maps/liebian.jpg' },
+      { id: 'map3', name: '隐世修所', image: 'https://valorant-strategy.oss-cn-heyuan.aliyuncs.com/images/maps/yinshi.jpg' },
+      { id: 'map4', name: '亚海悬城', image: 'https://valorant-strategy.oss-cn-heyuan.aliyuncs.com/images/maps/yahai.jpg' },
+      { id: 'map5', name: '莲华古城', image: 'https://valorant-strategy.oss-cn-heyuan.aliyuncs.com/images/maps/lianhua.jpg' },
+      { id: 'map6', name: '微风岛屿', image: 'https://valorant-strategy.oss-cn-heyuan.aliyuncs.com/images/maps/weifeng.jpg' },
+      { id: 'map7', name: '深海明珠', image: 'https://valorant-strategy.oss-cn-heyuan.aliyuncs.com/images/maps/shenhai.jpg' },
+      { id: 'map8', name: '森寒冬港', image: 'https://valorant-strategy.oss-cn-heyuan.aliyuncs.com/images/maps/senhan.jpg' },
+      { id: 'map9', name: '盐海矿镇', image: 'https://valorant-strategy.oss-cn-heyuan.aliyuncs.com/images/maps/yanhai.png' },
+      { id: 'map10', name: '日落之城', image: 'https://valorant-strategy.oss-cn-heyuan.aliyuncs.com/images/maps/riluo.png' },
+      { id: 'map11', name: '源工重镇', image: 'https://valorant-strategy.oss-cn-heyuan.aliyuncs.com/images/maps/yuangong.jpg' },
+      { id: 'map12', name: '幽邃地窟', image: 'https://valorant-strategy.oss-cn-heyuan.aliyuncs.com/images/maps/yousui.png' }
     ];
     this.setData({
       maps: mockMaps
